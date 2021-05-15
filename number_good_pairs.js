760d2f74ff2da5d08fa632a3168067e699c14a47
Jaskaran Singh <jaskaran1523@gmail.com>
2021-05-14 17:19:09 -0700

    Create number_good_pairs.js

---
+++ number_good_pairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[i] === nums[j])
+                count++;
+        }
+    }
+    
+    return count;
+};
